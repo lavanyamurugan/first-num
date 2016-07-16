7f48930f0f363e0e18e430ffad0948e4c7081353
lavanyamurugan <lavanyamkala@gmail.com>
2016-07-16 15:05:47 +0530

    Create number.c
    finish

---
+++ number.c
@@ -0,0 +1,16 @@
+#include<studio.h>
+void main()
+{
+int a,i;
+printf("enter the number");
+scanf("%d",&a);
+for(i=0;i<=a:i++)
+if(i>o){
+printf("the number is positive",i);
+}
+elseif(i<0)
+{
+printf("the number is negative",i);
+}
+if(i=0)
+printf("the number is zero",i);
