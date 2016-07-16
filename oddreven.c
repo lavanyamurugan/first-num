95190348c8b14a3ef0f1d7cda035054cb87202f5
lavanyamurugan <lavanyamkala@gmail.com>
2016-07-16 15:12:47 +0530

    Create oddreven.c
    finish

---
+++ oddreven.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+int a,i;
+printf("the number is");
+scanf("%d",&a);
+for(i=0;i<=a;i++)
+if(i%2==0)
+printf("the number is even",i)
+{
+else
+if(i%2!==0)
+printf("the number is odd",i)
+}
+}
